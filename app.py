ee1678446d6152d13cf5b7c48f7333ae885af011
White <James.White@gartner.com>
2019-04-02 20:09:55 -0400

    changing query to expedite loading

--- app.py
+++ app.py
@@ -60,39 +60,40 @@ def crime_data():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    remote_crime_data = pd.read_sql("SELECT * FROM crime_incidents_all", conn)
-    return(remote_crime_data.to_dict(orient="records"))
+    remote_crime_data = pd.read_sql("SELECT CCN,CENSUS_TRACT,END_DATE,LATITUDE,LONGITUDE,METHOD,OFFENSE,PSA,REPORT_DAT,SHIFT,START_DATE,WARD FROM crime_incidents_all", conn)
+    #print(remote_crime_data.to_dict(orient="records"))
+    return(jsonify(remote_crime_data.to_dict(orient="records")))
 
 
 
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
+#@app.route("/metadata/<sample>")
+#def sample_metadata(sample):
+#    """Return the MetaData for a given sample."""
+#    sel = [
+#        Samples_Metadata.sample,
+#        Samples_Metadata.ETHNICITY,
+#        Samples_Metadata.GENDER,
+#        Samples_Metadata.AGE,
+#        Samples_Metadata.LOCATION,
+#        Samples_Metadata.BBTYPE,
+#        Samples_Metadata.WFREQ,
+#    ]
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+#    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
 
     # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
+#    sample_metadata = {}
+#    for result in results:
+#        sample_metadata["sample"] = result[0]
+#        sample_metadata["ETHNICITY"] = result[1]
+#        sample_metadata["GENDER"] = result[2]
+#        sample_metadata["AGE"] = result[3]
+#        sample_metadata["LOCATION"] = result[4]
+#        sample_metadata["BBTYPE"] = result[5]
+#        sample_metadata["WFREQ"] = result[6]
 
-    print(sample_metadata)
-    return jsonify(sample_metadata)
+#    print(sample_metadata)
+#    return jsonify(sample_metadata)
 
 
 @app.route("/samples/<sample>")
