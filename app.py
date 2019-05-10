26ad55ccf03a9ba20eb8140c1d50188a138a6eb2
White <James.White@gartner.com>
2019-05-09 20:44:39 -0400

    push

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def index():
 #reena's endpoint
 @app.route("/ward_crimes")
 def line_chart():
-    return render_template("line_chart.html")
+    return render_template("index3.html")
 
 @app.route("/data")
 def crime_data():
